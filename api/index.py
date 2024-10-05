a898c7e791217235bb93acf18a74f0dfdb8ddfec
antsh3k <antshek@hotmail.com>
2024-10-05 18:32:31 +0100

    add env

--- api/index.py
+++ api/index.py
@@ -1,4 +1,8 @@
+from dotenv import load_dotenv
+import os
 from fastapi import FastAPI
+
+load_dotenv()
 
 ### Create FastAPI instance with custom docs and openapi url
 app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
